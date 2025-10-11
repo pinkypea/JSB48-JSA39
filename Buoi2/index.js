36b3c2fb7ee9aeb693e16b297bcc6ef9ad61bdda
unknown <daotrungkien2k2@gmail.com>
2025-10-11 10:00:36 +0700

    Buổi 2: Sức mạnh của ngôn ngữ lập trình Javascript

---
+++ Buoi2/index.js
@@ -0,0 +1,70 @@
+let age = 25;
+console.log(age);
+
+let winners = 2;
+let name = 'Duke';
+let isEligible = true;
+
+// Khai báo 6 biến để lưu trữ thông tin bản thân:
+// - first name
+// - last name
+// - age
+// - hometown
+// - email
+// - hobby
+
+// let first_name = "Kiên";
+// let last_name = "Đào"
+// let age = 25;
+// let hometown = "Hanoi";
+// let email = "daotrungkien@gmail.com";
+// let hobby = "Playing badminton"
+
+// Khai báo biến lưu trữ kiểu dữ liệu number
+let score = 10;
+let price = 100.5;
+
+// Khai báo biến lưu trữ kiểu dữ liệu string
+let school = "MindX";
+let major = "Technology";
+
+// Khai báo biến lưu trữ kiểu dữ liệu boolean
+let is_empty = true;
+let has_completed = false;
+
+// Khai báo biến lưu trữ kiểu dữ liệu undefined
+let result;
+
+// Khai báo biến lữu trữ kiểu dữ liệu null
+let selected_item = null;
+
+// Gộp chuỗi
+let first_name = "Kiên";
+let middle_name = "Trung";
+let last_name = "Đào";
+let full_name = last_name + " " + middle_name + " " + first_name;
+console.log(full_name);
+
+// Đếm số ký tự trong string
+let str = "Hello world!!!";
+console.log("Số ký tự trong chuỗi là", str.length);
+
+// Tách chuỗi
+let data = "apple,banana,orange";
+let sub_string = data.split(",");
+console.log(sub_string);
+
+// Viết hoa string
+let text = "hello world";
+let upper_text = text.toUpperCase();
+console.log(upper_text);
+
+// Viết thường string
+let text1 = "HELLO WORLD";
+let lower_text = text1.toLowerCase();
+console.log(lower_text);
+
+// Loại bỏ những khoảng trắng thừa ở trước/sau string
+let username = "     kiendao       ";
+let trimmed_username = username.trim();
+console.log(trimmed_username);
