0b46b61bffa98f50a01718c871d1422103d963ba
unknown <daotrungkien2k2@gmail.com>
2026-03-20 19:42:04 +0700

    fix function add to cart

--- How to use Firebase/index.js
+++ How to use Firebase/index.js
@@ -1,47 +1,42 @@
 function updateProductOnCart(productId) {
     db.collection("products").doc(productId).update({
-        "onCart": true
+        onCart: true
     })
     .then(() => {
         alert("Sản phẩm đã được thêm vào giỏ hàng");
     })
-    .catch ((error) => {
+    .catch((error) => {
         console.log("Lỗi không thêm được sản phẩm vào giỏ hàng", error);
-    })
+    });
 }
 
 function loadProducts(){
     const products_container = document.querySelector("#products-container");
+
     db.collection("products").get().then((querySnapshot) => {
         const docs = querySnapshot.docs;
+
         for (let i = 0; i < docs.length; i++) {
-            // Lấy ra các sản phẩm và lưu vào biến doc và product
             const doc = docs[i];
             const product = doc.data();
 
-            // Sử dụng DOM để hiển thị thông tin các sản phẩm
             const productElement = document.createElement("div");
             productElement.classList.add("product");
 
             productElement.innerHTML = `
                 <img src="${product.image}">
                 <p>Name: ${product.name}</p>
-                <p>Price: ${product.price}VND</p>
-                <button class="add-to-cart-btn" data-id = "${doc.id}">Add to cart</button>
-                `;
+                <p>Price: ${product.price} VND</p>
+                <button class="add-to-cart-btn">Add to cart</button>
+            `;
 
-            // Gán sự kiện cho các nút "Add to cart"
-            const buttons = document.querySelectorAll(".add-to-cart-btn");
-            for (let i = 0; i < buttons.length; i++){
-                buttons[i].addEventListener('click', () => {
-                    const productId = buttons[i].getAttribute('data-id');
-                    updateProductOnCart(productId);
-                })
-            }
-            
+            const button = productElement.querySelector(".add-to-cart-btn");
+            button.addEventListener('click', () => {
+                updateProductOnCart(doc.id);
+            });
             products_container.appendChild(productElement);
         }
-});
+    });
 }
 
 
